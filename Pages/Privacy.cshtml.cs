5c58b44db4689dbd6324b911ae1ebc072b939237
Xavier <xavieruxchamp@gmail.com>
2023-05-17 14:33:56 -0700

    Add project files.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace TAVS02E1___Final_Assignment.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+//My animation //
+
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
+
+// Functions to be called onmouseover and onmouseout to chenge the title
+function newColor() {
+    document.getElementById("the_title").style.color = "magenta";
+}
+
+function oldColor() {
+    document.getElementById("the_title").style.color = "green";
+}
